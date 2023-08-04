d325cc8548b93dd51d0217b22146e38f07dc3db5
Ivan <i.a.konuhov@gmail.com>
2023-08-04 17:54:06 +0700

    lesson-11: Рефакторинг

--- lesson-11/Users-dictionary/main.c
+++ lesson-11/Users-dictionary/main.c
@@ -12,22 +12,103 @@
 #include <string.h>
 
 
+struct Subscriber {
+    char name[15];
+    char phoneNumber[15];
+};
+
+
+//  Добавить абонента
+void insert(struct Subscriber *subscribers) {
+
+    printf("\nВведите имя абонента\n");
+    char name[15] = {};
+    scanf("%15s", &name);
+
+    printf("\nВведите номер абонента\n");
+    char phoneNumber[15] = {};
+    scanf("%15s", &phoneNumber);
+
+
+    //  Поиск пустого места в массиве и вставка в него элемента
+    for(int i = 0; i < 100; i++) {
+
+        if(strcmp(subscribers[i].name, "") == 0 && strcmp(subscribers[i].phoneNumber, "") == 0) {
+
+            strcpy(subscribers[i].name, name);
+            strcpy(subscribers[i].phoneNumber, phoneNumber);
+
+            break;
+        }
+    }
+}
+
+//  Вывести список
+void showAll(struct Subscriber *subscribers) {
+
+    printf("\n\n");
+
+    for (int i = 0; i < 100; i++) {
+
+        if (strcmp(subscribers[i].name, "") != 0) {
+
+            printf("%d. %s %s\n", i + 1, subscribers[i].name, subscribers[i].phoneNumber);
+        }
+    }
+
+    printf("\n\n");
+}
+
+//  Поиск по имени
+void findSubscriber(struct Subscriber *subscribers) {
+
+    printf("\nВведите имя абонента, номер(а) которого нужно найти\n");
+    char nameForFind[15] = {};
+    scanf("%15s", &nameForFind);
+
+    printf("\n");
+
+    for (int i = 0; i < 100; i++) {
+
+        if(strcmp(nameForFind, subscribers[i].name) == 0) {
+
+            printf("%s\n", subscribers[i].phoneNumber);
+        }
+    }
+
+    printf("\n");
+}
+
+//  Удалить абонента
+void deleteSubscriber(struct Subscriber *subscribers) {
+
+    printf("\nВведите имя абонента, подлежащего удалению\n");
+    char nameForDeletion[15] = {};
+    scanf("%15s", &nameForDeletion);
+
+    for (int i = 0; i < 100; i++) {
+
+        if (strcmp(nameForDeletion, subscribers[i].name) == 0) {
+
+            for (int j = 0; j < 15; j++) {
+
+                subscribers[i].name[j] = 0;
+                subscribers[i].phoneNumber[j] = 0;
+            }
+        }
+    }
+}
+
+
 int main(void) {
 
-    struct Subscriber {
-
-        char name[15];
-        char phoneNumber[15];
-    };
-
     struct Subscriber subscribers[100];
-
 
     char isRunning = 1;
     char choice;
 
 
-    //  Очистка массива перед использованием. Какой ужас
+    //  Очистка массива перед использованием.
     for (int i = 0; i < 100; i++) {
 
         for (int j = 0; j < 15; j++) {
@@ -35,100 +116,40 @@ int main(void) {
             subscribers[i].phoneNumber[j] = 0;
         }
     }
-    
+
 
     while(isRunning){
 
         printf("\nВыберите команду:\n1. Добавить абонента.\n2. Вывести список абонентов.\n3. Найти абонента по имени\n4. Удалить абонента\n5. Выйти из справочника\n");
         scanf("%s", &choice);
         isRunning = 1;
-        
+
 
         switch (choice) {
-        case '1':
+            case '1':
+                insert(&subscribers);
+                break;
 
-            printf("Введите имя абонента\n");
-            char name[15] = {};
-            scanf("%15s", &name);
+            case '2':
+                showAll(&subscribers);
+                break;
 
-            printf("Введите номер абонента\n");
-            char phoneNumber[15] = {};
-            scanf("%15s", &phoneNumber);
+            case '3':
+                findSubscriber(&subscribers);
+                break;
 
+            case '4':
+                deleteSubscriber(&subscribers);
+                break;
 
-            //  Поиск пустого места в массиве и вставка в него элемента
-            for(int i = 0; i < 100; i++) {
-                
-                if(strcmp(subscribers[i].name, "") == 0 && strcmp(subscribers[i].phoneNumber, "") == 0) {
-                    
-                    strcpy(subscribers[i].name, name);
-                    strcpy(subscribers[i].phoneNumber, phoneNumber);
-                    
-                    break;
-                }
-            }
+            case '5':
+                //  Выход
+                isRunning = 0;
+                break;
 
-            break;
-
-        case '2':
-
-            printf("\n\n");
-
-            for (int i = 0; i < 100; i++) {
-                
-                if (strcmp(subscribers[i].name, "") != 0) {
-                    printf("%d. %s %s\n", i + 1, subscribers[i].name, subscribers[i].phoneNumber);
-                }   
-            }
-            
-            printf("\n\n");
-
-            break;
-
-        case '3':
-
-            printf("\nВведите имя абонента, номер(а) которого нужно найти\n");
-            char nameForFind[15] = {};
-            scanf("%15s", &nameForFind);
-
-            for (int i = 0; i < 100; i++) {
-                
-                if(strcmp(nameForFind, subscribers[i].name) == 0) {
-
-                    printf("%s\n", subscribers[i].phoneNumber);
-                }
-            }
-            
-            break;
-
-        case '4':
-
-            printf("\nВведите имя абонента, подлежащего удалению\n");
-            char nameForDeletion[15] = {};
-            scanf("%15s", &nameForDeletion);
-            
-            for (int i = 0; i < 100; i++) {
-                
-                if (strcmp(nameForDeletion, subscribers[i].name) == 0) {
-                    
-                    for (int j = 0; j < 15; j++) {
-
-                        subscribers[i].name[j] = 0;
-                        subscribers[i].phoneNumber[j] = 0;
-                    }
-                }
-            }
-
-            break;
-
-        case '5':
-
-            isRunning = 0;
-            break;
-
-        default:
-            printf("Ошибка ввода\n");
-            break;
+            default:
+                printf("Ошибка ввода\n");
+                break;
         }
     }
 }
